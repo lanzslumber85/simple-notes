7a6b2c34811db33d47fd7cab632348810a6c83b5
Mohd Azlan Hashim <78134294+lanzslumber85@users.noreply.github.com>
2021-03-15 09:09:54 +0800

    Creating Login page

---
+++ src/containers/pages/Login/index.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Login extends React.Component {
+    render() {
+        return (
+            <div>
+                <p>Login Page</p>
+                <button>Go to Register</button>
+                <button>Go to Dashboard</button>
+            </div>
+        );
+    }
+}
+
+export default Login;
