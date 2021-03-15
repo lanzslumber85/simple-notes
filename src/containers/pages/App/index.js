b4c1d10a4423a9aaefe9f64e58c8d637cd350445
Mohd Azlan Hashim <78134294+lanzslumber85@users.noreply.github.com>
2021-03-15 14:40:13 +0800

    Setup redux

--- src/containers/pages/App/index.js
+++ src/containers/pages/App/index.js
@@ -1,18 +1,49 @@
-import { BrowserRouter as Router, Link, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 import Dashboard from "./../Dashboard/index";
 import Login from "./../Login/index";
 import Register from "./../Register/index";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+
+const initialState = {
+    popup: false,
+    isLogin: false,
+};
+
+const reducer = (state = initialState, action) => {
+    if (action.type === "CHANGE_POPUP") {
+        return {
+            ...state,
+            popup: action.value,
+        };
+    }
+
+    if (action.type === "CHANGE_ISLOGIN") {
+        return {
+            ...state,
+            isLogin: action.value,
+        };
+    }
+
+    return state;
+};
+
+let store = createStore(reducer);
+
+store.subscribe(() => console.log(store.getState()));
 
 function App() {
     return (
-        <Router>
-            <div className="App">
-                <Route path="/" exact component={Dashboard} />
-                <Route path="/login" component={Login} />
-                <Route path="/register" component={Register} />
-            </div>
-        </Router>
+        <Provider store={store}>
+            <Router>
+                <div className="App">
+                    <Route path="/" exact component={Dashboard} />
+                    <Route path="/login" component={Login} />
+                    <Route path="/register" component={Register} />
+                </div>
+            </Router>
+        </Provider>
     );
 }
 
