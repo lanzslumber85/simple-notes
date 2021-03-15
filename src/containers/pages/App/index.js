39d79c6ce852e8a4e493fb4b4e2dba07989d62a2
Mohd Azlan Hashim <78134294+lanzslumber85@users.noreply.github.com>
2021-03-15 15:22:38 +0800

    Import store from redux folder

--- src/containers/pages/App/index.js
+++ src/containers/pages/App/index.js
@@ -3,33 +3,8 @@ import "./App.css";
 import Dashboard from "./../Dashboard/index";
 import Login from "./../Login/index";
 import Register from "./../Register/index";
-import { createStore } from "redux";
 import { Provider } from "react-redux";
-
-const initialState = {
-    popup: false,
-    isLogin: false,
-};
-
-const reducer = (state = initialState, action) => {
-    if (action.type === "CHANGE_POPUP") {
-        return {
-            ...state,
-            popup: action.value,
-        };
-    }
-
-    if (action.type === "CHANGE_ISLOGIN") {
-        return {
-            ...state,
-            isLogin: action.value,
-        };
-    }
-
-    return state;
-};
-
-let store = createStore(reducer);
+import { store } from "../../../config/redux";
 
 store.subscribe(() => console.log(store.getState()));
 
