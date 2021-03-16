998da8213d4f4e4e847bc8f259bad420afad2b18
Mohd Azlan Hashim <78134294+lanzslumber85@users.noreply.github.com>
2021-03-16 14:34:06 +0800

    Update action file

--- src/config/redux/action/index.js
+++ src/config/redux/action/index.js
@@ -1,8 +1,31 @@
+import firebase from "./../../firebase/index";
+
 export const getAction = () => dispatch => {
     setTimeout(() => {
         return dispatch({
             type: "CHANGE_USERNAME",
             value: "01000001 01111010 01101100 01100001 01101110",
         });
-    }, 2000);
+    }, 4000);
+};
+
+export const registerUserIntoFirebase = data => dispatch => {
+    dispatch({ type: "CHANGE_LOADING", value: true });
+
+    return firebase
+        .auth()
+        .createUserWithEmailAndPassword(data.email, data.password)
+        .then(userCredential => {
+            // Signed in
+            var user = userCredential.user;
+            console.log("success: ", user);
+            dispatch({ type: "CHANGE_LOADING", value: false });
+        })
+        .catch(error => {
+            var errorCode = error.code;
+            var errorMessage = error.message;
+            console.log("errorCode:", errorCode);
+            console.log("errorMessage:", errorMessage);
+            dispatch({ type: "CHANGE_LOADING", value: false });
+        });
 };
