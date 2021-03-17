2294a5d797866bda9c94c076da42d64efaf4fc65
Mohd Azlan Hashim <78134294+lanzslumber85@users.noreply.github.com>
2021-03-17 14:15:12 +0800

    Update Dashboard component

--- src/containers/pages/Dashboard/index.js
+++ src/containers/pages/Dashboard/index.js
@@ -1,15 +1,75 @@
 import React from "react";
+import { connect } from "react-redux";
+import { addDataToAPI } from "../../../config/redux/action";
+import "./Dashboard.scss";
 
 class Dashboard extends React.Component {
+    state = {
+        title: "",
+        content: "",
+        date: "",
+    };
+
+    handleSaveNotes = () => {
+        const { title, content } = this.state;
+        const { saveNotes } = this.props;
+
+        const data = {
+            title: title,
+            content: content,
+            date: new Date().getTime(),
+            userID: this.props.userData.uid,
+        };
+
+        saveNotes(data);
+
+        console.table(data);
+    };
+
+    onInputChange = (e, type) => {
+        this.setState({
+            [type]: e.target.value,
+        });
+    };
+
     render() {
+        const { title, content, date } = this.state;
         return (
-            <div>
-                <p>Dashboard Page</p>
-                <button>Go to Register</button>
-                <button>Go to Dashboard</button>
+            <div className="container">
+                <div className="input-form">
+                    <input
+                        className="input-title"
+                        placeholder="title"
+                        value={title}
+                        onChange={e => this.onInputChange(e, "title")}
+                    />
+                    <textarea
+                        className="input-content"
+                        placeholder="content"
+                        value={content}
+                        onChange={e => this.onInputChange(e, "content")}
+                    ></textarea>
+                    <button className="save-btn" onClick={this.handleSaveNotes}>
+                        Save
+                    </button>
+                </div>
+                <hr />
+                <div className="card-content">
+                    <p className="title">Title</p>
+                    <p className="date">21 Sep 2019</p>
+                    <p className="content">Content Notes</p>
+                </div>
             </div>
         );
     }
 }
 
-export default Dashboard;
+const reduxState = state => {
+    return { userData: state.user };
+};
+
+const reduxDispatch = dispatch => {
+    return { saveNotes: data => dispatch(addDataToAPI(data)) };
+};
+
+export default connect(reduxState, reduxDispatch)(Dashboard);
