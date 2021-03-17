994ad00b49b0d4c6a6aee98753cb07d0da40f714
Mohd Azlan Hashim <78134294+lanzslumber85@users.noreply.github.com>
2021-03-17 14:12:51 +0800

    Update action

--- src/config/redux/action/index.js
+++ src/config/redux/action/index.js
@@ -1,4 +1,4 @@
-import firebase from "./../../firebase/index";
+import firebase, { database } from "./../../firebase/index";
 
 export const getAction = () => dispatch => {
     setTimeout(() => {
@@ -10,22 +10,62 @@ export const getAction = () => dispatch => {
 };
 
 export const registerUserIntoFirebase = data => dispatch => {
-    dispatch({ type: "CHANGE_LOADING", value: true });
+    return new Promise((resolve, reject) => {
+        dispatch({ type: "CHANGE_LOADING", value: true });
+        firebase
+            .auth()
+            .createUserWithEmailAndPassword(data.email, data.password)
+            .then(res => {
+                console.log("success: ", res);
+                dispatch({ type: "CHANGE_LOADING", value: false });
+                resolve(true);
+            })
+            .catch(error => {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                console.log("errorCode:", errorCode);
+                console.log("errorMessage:", errorMessage);
+                dispatch({ type: "CHANGE_LOADING", value: false });
+                reject(false);
+            });
+    });
+};
 
-    return firebase
-        .auth()
-        .createUserWithEmailAndPassword(data.email, data.password)
-        .then(userCredential => {
-            // Signed in
-            var user = userCredential.user;
-            console.log("success: ", user);
-            dispatch({ type: "CHANGE_LOADING", value: false });
-        })
-        .catch(error => {
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            console.log("errorCode:", errorCode);
-            console.log("errorMessage:", errorMessage);
-            dispatch({ type: "CHANGE_LOADING", value: false });
-        });
+export const loginUser = data => dispatch => {
+    return new Promise((resolve, reject) => {
+        dispatch({ type: "CHANGE_LOADING", value: true });
+        firebase
+            .auth()
+            .signInWithEmailAndPassword(data.email, data.password)
+            .then(res => {
+                // console.log("success: ", res);
+                const dataUser = {
+                    email: res.user.email,
+                    uid: res.user.uid,
+                    emailVerified: res.user.emailVerified,
+                    refreshToken: res.user.refreshToken,
+                };
+                dispatch({ type: "CHANGE_LOADING", value: false });
+                dispatch({ type: "CHANGE_ISLOGIN", value: true });
+                dispatch({ type: "CHANGE_USERNAME", value: dataUser });
+                resolve(dataUser);
+            })
+            .catch(error => {
+                var errorCode = error.code;
+                var errorMessage = error.message;
+                console.log("errorCode:", errorCode);
+                console.log("errorMessage:", errorMessage);
+                dispatch({ type: "CHANGE_LOADING", value: false });
+                dispatch({ type: "CHANGE_ISLOGIN", value: false });
+                reject(false);
+            });
+    });
+};
+
+export const addDataToAPI = data => dispatch => {
+    database.ref("notes/" + data.userID).push({
+        title: data.title,
+        content: data.content,
+        date: data.date,
+    });
 };
