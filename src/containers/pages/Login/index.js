5ce77020bcaa61674b53913c65999bcb706ef542
Mohd Azlan Hashim <78134294+lanzslumber85@users.noreply.github.com>
2021-03-17 14:15:23 +0800

    Update Login component

--- src/containers/pages/Login/index.js
+++ src/containers/pages/Login/index.js
@@ -1,31 +1,74 @@
 import React from "react";
 import { connect } from "react-redux";
-import { getAction } from "./../../../config/redux/action/index";
+import Button from "./../../../components/atoms/Button/index";
+import { loginUser } from "./../../../config/redux/action/index";
 
 class Login extends React.Component {
-    changeUser = () => {
-        const { changeUsername } = this.props;
-        return changeUsername();
+    state = {
+        email: "",
+        password: "",
+    };
+
+    handleChangeText = e => {
+        this.setState({
+            [e.target.id]: e.target.value,
+        });
+    };
+
+    handleLoginSubmit = async () => {
+        const { email, password } = this.state;
+        const { history } = this.props;
+        const res = await this.props
+            .loginUser({ email, password })
+            .catch(err => err);
+        if (res) {
+            console.log("Login Success...", res);
+            this.setState({ email: "", password: "" });
+            history.push("/");
+        } else {
+            console.log("Login failed...");
+        }
     };
 
     render() {
-        const { userName } = this.props;
         return (
-            <div>
-                <p>Login Page {userName}</p>
-                <button onClick={this.changeUser}>Display Username</button>
-                <button>Go to Dashboard</button>
+            <div className="auth-container">
+                <div className="auth-card">
+                    <p className="auth-title">Login Page</p>
+                    <input
+                        className="input"
+                        id="email"
+                        type="email"
+                        value={this.state.email}
+                        placeholder="Email"
+                        onChange={this.handleChangeText}
+                    ></input>
+                    <input
+                        className="input"
+                        id="password"
+                        type="password"
+                        value={this.state.password}
+                        placeholder="Password"
+                        onChange={this.handleChangeText}
+                    ></input>
+
+                    <Button
+                        onClick={this.handleLoginSubmit}
+                        title="Login"
+                        loading={this.props.isLoading}
+                    />
+                </div>
             </div>
         );
     }
 }
 
 const reduxState = state => {
-    return { userName: state.user };
+    return { isLoading: state.isLoadingWillBeTrigger };
 };
 
 const reduxDispatch = dispatch => {
-    return { changeUsername: () => dispatch(getAction()) };
+    return { loginUser: data => dispatch(loginUser(data)) };
 };
 
 export default connect(reduxState, reduxDispatch)(Login);
